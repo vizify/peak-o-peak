d0a1e54333936243f10dc50b723b48f5b860c330
Kelly <kelly.xie@tuhh.de>
2026-05-30 20:59:19 +0200

    box debug

--- scripts/box.gd
+++ scripts/box.gd
@@ -1,23 +1,31 @@
 extends RigidBody2D
 
-
 @export var required_players: int = 2
 var players_pushing: int = 0
 
 func _ready():
-	freeze = true
-
+	# Start completely unfrozen so it drops immediately
+	freeze = false
+	sleeping = false
+	lock_rotation = true
 
 func _physics_process(delta):
+	# Get a list of everything physically touching the box right now
+	var colliding_bodies = get_colliding_bodies()
+	
+	var is_touching_ground = false
+	for body in colliding_bodies:
+		if not body.is_in_group("player"):
+			is_touching_ground = true
+			break
+
+	# IF 2+ players are pushing, ALWAYS let it move
 	if players_pushing >= required_players:
 		freeze = false
+		sleeping = false
 	else:
-		freeze = true
-
-func _on_area_2d_body_entered(body):
-	if body.is_in_group("player"):
-		players_pushing += 1
-
-func _on_area_2d_body_exited(body):
-	if body.is_in_group("player"):
-		players_pushing -= 1
+		# If there aren't enough players, ONLY freeze it if it has landed on the ground
+		if is_touching_ground:
+			freeze = true
+		else:
+			freeze = false # Keeps it falling through the air if it hasn't hit the ground yet
